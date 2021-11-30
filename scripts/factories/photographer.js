6df975ad659cdf957562ee2ef52b5922212a896f
Sidonie <sidonie.jung@quickms.fr>
2021-11-30 16:01:01 +0100

    maj accessibilité dans les pages html

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,9 +4,13 @@ function photographerFactory(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
+        // TODO penser à vérifier l'accessibilité pour les articles
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        // ajouter alt pour img
         img.setAttribute("src", picture)
+        // passer en h1
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
