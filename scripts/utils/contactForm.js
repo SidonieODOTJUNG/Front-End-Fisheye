26713e8cd1544cd7589f8adfa9f38e969f84308c
Sidonie <sidonie.jung@quickms.fr>
2022-03-03 15:02:31 +0100

    maj echap clavier modal + envoi des data console

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -5,6 +5,11 @@
 /****************************/
 
 /* eslint-disable no-unused-vars */
+function listenEscape (e) {
+  if (e.code === 'Escape') { closeModal() }
+}
+document.addEventListener('keydown', listenEscape)
+
 function displayModal () {
   const modal = document.getElementById('contact_modal')
   modal.style.display = 'block'
@@ -13,4 +18,18 @@ function displayModal () {
 function closeModal () {
   const modal = document.getElementById('contact_modal')
   modal.style.display = 'none'
+}
+
+document.getElementById('contactForm').addEventListener('submit', sendContact)
+
+function sendContact (event) {
+  event.preventDefault()
+  const firstName = document.getElementById('firstName').value
+  const lastName = document.getElementById('lastName').value
+  const mail = document.getElementById('mail').value
+  const message = document.getElementById('message').value
+
+  console.log(firstName, lastName, mail, message)
+
+  closeModal()
 }
